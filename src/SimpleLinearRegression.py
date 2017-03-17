c3a6ea64aa14a7c4baebce18cefb85007e112b7d
nickbuker <nickbuker@gmail.com>
2017-03-17 14:01:29 -0700

    add score method

--- src/SimpleLinearRegression.py
+++ src/SimpleLinearRegression.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scoring import R2
 
 
 class SimpleLinearRegression(object):
@@ -8,16 +9,36 @@ class SimpleLinearRegression(object):
 
     def fit(self, x, y):
         """
-        Input: numpy arrays x and y
+        Input: numpy arrays of training data x and y
         Ouput: prints linear equation for trained model
         """
         self.x_train, self.y_train = x, y
         self.x_bar, self.y_bar = np.mean(x), np.mean(y)
         self.b1 = self._find_b1()
         self.b0 = self._find_b0()
-        self.RSS = self._find_RSS(train=True)
         print 'y_hat = {} + {} * x'.format(self.b1, self.b0)
-        print 'RSS = {}'.format(self.RSS)
+
+    def predict(self, x):
+        """
+        Input: numpy array of test data x
+        Ouput: numpy array of predicted y
+        """
+        try:
+            self.y_hat = (self.b1 * x) + self.b0
+            return self.y_hat
+        except AttributeError:
+            print 'Please fit the model before making predictions.'
+
+    def score(self, y):
+        """
+        Input: numpy array of test data y
+        Ouput: R-squared score
+        """
+        try:
+            self.R2 = R2(y, self.y_hat)
+            return self.R2
+        except AttributeError:
+            print 'Please make a prediction before scoring.'
 
     def _find_b1(self):
         """
@@ -34,12 +55,3 @@ class SimpleLinearRegression(object):
         Output: intercept for linear model
         """
         return self.y_bar - self.b1 * self.x_bar
-
-    def _find_RSS(self, train=True):
-        """
-        Input: none
-        Output: residual sum of squares
-        """
-        if train:
-            y_hat = (self.b1 * self.x_train) + self.b0
-            return sum((self.y_train - y_hat) ** 2)
