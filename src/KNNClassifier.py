33cbbd6a22182f74c34df8bba2934117ddda47f3
nickbuker <nickbuker@gmail.com>
2017-03-19 19:30:25 -0700

    add distance and assignment methods

--- src/KNNClassifier.py
+++ src/KNNClassifier.py
@@ -13,12 +13,13 @@ class KNNClassifier(object):
         """
         self.X_train, self.y_train = X, y
 
-    def predict(self, X):
+    def predict(self, k, X):
         """
         Input: numpy array of X
         Output: numpy array of y_hat
         """
-        pass
+        self.k = k
+        return self._dist(X)
 
     def score(self, y):
         """
@@ -28,7 +29,19 @@ class KNNClassifier(object):
         pass
 
     def _dist(self, X):
-        pass
+        return np.apply_along_axis(self._row_dist, 1, X)
 
-    def _assign(self, distances):
-        pass
+    def _row_dist(self, row):
+        k_dist = []
+        for i, r in enumerate(self.X_train):
+            d = np.linalg.norm(row - r)
+            if len(k_dist) < self.k:
+                k_dist.append((d, self.y_train[i]))
+                k_dist.sort()
+            elif d < k_dist[-1][0]:
+                k_dist[-1] = (d, self.y_train[i])
+                k_dist.sort()
+        return self._assign(k_dist)
+
+    def _assign(self, k_dist):
+        return np.mean([n[1] for n in k_dist])
