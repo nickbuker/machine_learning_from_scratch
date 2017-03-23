010150e1ba960e293211e6811bb4b20a39f5a86e
nickbuker <nickbuker@gmail.com>
2017-03-23 08:33:46 -0700

    add comments and try except to Multiple Linear

--- src/MultipleLinearRegression.py
+++ src/MultipleLinearRegression.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scoring import R2
 
 
 class MultipleLinearRegression(object):
@@ -7,15 +8,42 @@ class MultipleLinearRegression(object):
         pass
 
     def fit(self, X, y):
-        self.X_train = X
-        self.y_train = y
-        self.betas = self._matix()
+        """
+        Input: numpy arrays of training data x and y
+        Ouput: prints betas for linear model
+        """
+        self.X = X
+        self.y = y
+        self.betas = self._find_betas()
         for i, beta in enumerate(self.betas):
-            print 'B{}:  {}'.format(i, beta)
+            print 'B{}: {}'.format(i, beta),
 
-    def predict(self):
-        pass
+    def predict(self, x):
+        """
+        Input: numpy array of test data x
+        Ouput: numpy array of predicted y
+        """
+        try:
+            self.y_hat = x.dot(self.betas)
+            return self.y_hat
+        except AttributeError:
+            print 'Please fit the model before making predictions.'
 
-    def _matrix(self):
-        # (X'X)^-1 X'Y
+    def score(self, y):
+        """
+        Input: numpy array of test data y
+        Ouput: R-squared score
+        """
+        try:
+            self.R2 = R2(y, self.y_hat)
+            return self.R2
+        except AttributeError:
+            print 'Please make a prediction before scoring.'
+
+    def _find_betas(self):
+        """
+        Input: none
+        Output: numpy array of betas
+        """
+        # solve for betas using (X'X)^-1 X'Y
         return np.linalg.inv(self.X.T.dot(self.X)).dot(self.X.T).dot(self.y)
