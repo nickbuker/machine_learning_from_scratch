24c34376e2f7f88198179162f149a5dfd4c9384b
Buker, Nick <Nick.Buker@nordstrom.com>
2017-08-15 10:43:50 -0700

    update LogisticRegression

--- src/LogisticRegression.py
+++ src/LogisticRegression.py
@@ -32,7 +32,12 @@ class LogisticRegression:
         -------
         None
         """
-        pass
+        self.betas = self._gradient_descent(X=X,
+                                            y=y,
+                                            intercept=intercept,
+                                            learning_rate=learning_rate,
+                                            convergence_change=converge_change,
+                                            max_iter=max_iter)
 
     def predict(self, X, prob=True, threshold=0.5):
         """ Makes probability or class predictions for test data
@@ -53,24 +58,37 @@ class LogisticRegression:
         numpy array
             probabilities or predictions for test data depending on the prob parameter
         """
-        pass
+        self.y_prob = self._logit(X, self.betas)
+        if prob:
+            return self.y_prob
+        else:
+            self.y_pred = np.zeros(len(self.y_prob))
+            # if prob in y_prob >= threshold, convert label to 1
+            np.place(self.y_pred, self.y_prob >= threshold, 1)
+            return self.y_pred
 
     def score(self, y, metric='log_loss'):
-        """
+        """ Scores the predictions
 
         Parameters
         ----------
         y : numpy array
             actual 0 and 1 class labels for test data
         metric : string
-
+            if 'log_loss' then returns log loss
+            if 'accuracy' then returns accuracy
 
         Returns
         -------
         float
             log loss or accuracy score depending on the metric parameter
         """
-        pass
+        if metric == 'log_loss':
+            return log_loss(y, self.y_prob)
+        elif metric == 'accuracy':
+            return accuracy(y, self.y_pred)
+        else:
+            print('valid scoring metrics are log_loss or accuracy')
 
     def _gradient_descent(self, X, y, intercept, learning_rate, convergence_change, max_iter):
         """ Solves for betas using gradient descent
@@ -101,8 +119,55 @@ class LogisticRegression:
             print('intercept not yet implemented')
             return
         else:
+            # initialize variables
             betas = np.zeros(X.shape[1])
-            # TODO continue writing me
+            y_prob = self._logit(X, betas)
+            loss = log_loss(y, y_prob)
+            change = 1
+            i = 1
+            # loop until convergence or max iterations are reached
+            while change > convergence_change and i <= max_iter:
+                old_loss = loss
+                betas = betas - (learning_rate * self._gradient(betas, X, y))
+                y_prob = self._logit(X, betas)
+                loss = log_loss(y, y_prob)
+                change = old_loss - loss
+                i += 1
+        if i == max_iter:
+            print('failed to reach convergence')
+        return betas
 
     def _logit(self, X, betas):
+        """
+
+        Parameters
+        ----------
+        X : numpy array
+            data
+        betas : numpy array
+            beta values for linear equation
+        Returns
+        -------
+        numpy array
+            probabilities of belonging to class 1
+        """
         return 1 / (1 + np.exp(-X.dot(betas)))
+
+    def _gradient(self, betas, X, y):
+        """ Calculates the gradient of the betas
+
+        Parameters
+        ----------
+        betas
+        X : numpy array
+            training data
+        y : : numpy array
+            actual 0 and 1 class labels for test data
+
+        Returns
+        -------
+        numpy array
+            gradient of betas
+        """
+        diffs = self._logit(X, betas) - y
+        return diffs.T.dot(X)
