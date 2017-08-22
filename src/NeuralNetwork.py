7316477dcc1b330a76a8a58bb6fb25bf84578e86
Buker, Nick <Nick.Buker@nordstrom.com>
2017-08-22 09:09:15 -0700

    update neural network predict method

--- src/NeuralNetwork.py
+++ src/NeuralNetwork.py
@@ -1,4 +1,4 @@
-from LogisticRegression import LogisticRegression
+import numpy as np
 
 
 class NeuralNetwork:
@@ -6,7 +6,8 @@ class NeuralNetwork:
     def __init__(self):
         pass
 
-    def fit(self, X, y, in_out_nodes, hidden_nodes):
+    def fit(self, X, y, in_nodes, hid_nodes, out_nodes, epochs=10000,
+            learning_rate=0.01, reg_factor=0.01):
         """ Takes in test data and trains model
 
         Parameters
@@ -15,15 +16,30 @@ class NeuralNetwork:
             training data
         y : numpy array
             labels for training data
-        in_out_nodes : int
-            number of nodes in input and output layers
-        hidden_nodes
+        in_nodes : int
+            number of nodes in the input layer
+        hid_nodes : int
             number of nodes in hidden layer
+        out_nodes : int
+            number of nodes in output layer
+        epochs : int
+            number of full passes through training data (default value 10000)
+        learning_rate : float
+            learning rate for gradient descent (default value 0.01)
+        reg_factor : float
+            regularization strength (default value 0.01)
+
         Returns
         -------
         None
         """
-        pass
+        # initialize parameters to some random values
+        weights1 = np.random.randn(in_nodes, hid_nodes) / np.sqrt(in_nodes)
+        betas1 = np.zeros((1, hid_nodes))
+        weights2 = np.random.randn(hid_nodes, out_nodes) / np.sqrt(hid_nodes)
+        betas2 = np.zeros(1, out_nodes)
+        model = {'weights1': weights1, 'betas1': betas1,
+                 'weights2': weights2, 'betas2': betas2}
 
     def predict(self, X, prob=True):
         """ Makes probability or class predictions for test data
@@ -41,4 +57,16 @@ class NeuralNetwork:
         numpy array
             probabilities or class predictions for test data depending on the prob parameter
         """
+        pass
+
+    def _gradient_descent(self, learning_rate, reg_parameter):
+        pass
+
+    def _forward_propagation(self):
+        pass
+
+    def _loss_function(self):
+        pass
+
+    def _back_propagation(self):
         pass
