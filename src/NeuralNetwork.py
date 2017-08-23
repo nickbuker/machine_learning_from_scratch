557089b20acfae08a9491604a2ec34563ac00343
Buker, Nick <Nick.Buker@nordstrom.com>
2017-08-23 08:37:30 -0700

    update docstrings and fit method

--- src/NeuralNetwork.py
+++ src/NeuralNetwork.py
@@ -7,7 +7,7 @@ class NeuralNetwork:
         pass
 
     def fit(self, X, y, in_nodes, hid_nodes, out_nodes, epochs=10000,
-            learning_rate=0.01, reg_factor=0.01):
+            learning_rate=0.01, reg_factor=0.01, random_seed=97):
         """ Takes in test data and trains model
 
         Parameters
@@ -28,18 +28,25 @@ class NeuralNetwork:
             learning rate for gradient descent (default value 0.01)
         reg_factor : float
             regularization strength (default value 0.01)
+        random_seed : int
+            optional random seed for initial weights generated
 
         Returns
         -------
         None
         """
         # initialize parameters to some random values
+        np.random.seed(random_seed)
+        nodes = (in_nodes, hid_nodes, out_nodes)
         weights1 = np.random.randn(in_nodes, hid_nodes) / np.sqrt(in_nodes)
         betas1 = np.zeros((1, hid_nodes))
         weights2 = np.random.randn(hid_nodes, out_nodes) / np.sqrt(hid_nodes)
         betas2 = np.zeros(1, out_nodes)
-        model = {'weights1': weights1, 'betas1': betas1,
-                 'weights2': weights2, 'betas2': betas2}
+        self.model = {'weights1': weights1, 'betas1': betas1,
+                      'weights2': weights2, 'betas2': betas2,
+                      'nodes': nodes}
+        # use gradient descent to estimate weights and betas
+        self._gradient_descent(X, y, epochs, learning_rate, reg_factor)
 
     def predict(self, X, prob=True):
         """ Makes probability or class predictions for test data
@@ -59,14 +66,48 @@ class NeuralNetwork:
         """
         pass
 
-    def _gradient_descent(self, learning_rate, reg_parameter):
+    def _gradient_descent(self, X, y, epochs, learning_rate, reg_parameter):
+        """ Optimizes betas and weights using gradient descent
+
+        Parameters
+        ----------
+        X : numpy array
+            training data
+        y : numpy array
+            labels for training data
+        epochs : int
+            number of full passes through training data
+        learning_rate : float
+            learning rate for gradient descent (default value 0.01)
+        reg_parameter : float
+            regularization strength (default value 0.01)
+
+        Returns
+        -------
+        dict
+            trained model
+        """
         pass
 
     def _forward_propagation(self):
         pass
 
-    def _loss_function(self):
+    def _back_propagation(self):
         pass
 
-    def _back_propagation(self):
+    def score(self, X, y):
+        # TODO
+        """
+
+        Parameters
+        ----------
+        X : numpy array
+            test data
+        y : numpy array
+            actual class labels for test data
+
+        Returns
+        -------
+
+        """
         pass
