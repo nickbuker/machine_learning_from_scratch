a50f3d237a5e1d95d03908ad9e47246b14fab673
nickbuker <nickbuker@gmail.com>
2017-10-22 17:42:48 -0700

    commit initial dtr work

---
+++ src/DecisionTreeRegressor.py
@@ -0,0 +1,42 @@
+import numpy as np
+import pandas as pd
+from scoring import R2
+
+
+class DecisionTreeRegressor:
+
+    def __init__(self):
+        pass
+
+    def fit(self, X, y, max_depth):
+        self.rules = []
+        X = self._check_x_data_type(X)
+        X['y'] = y
+        X['y_hat'] = np.mean(y)
+        X = X.sort_values(by='y')
+        X = X.reset_index(drop=True)
+        data_cols = [col for col in X.columns if col not in ['y', 'y_hat']]
+        for _ in range(max_depth):
+            score = [0, data_cols[0], -1]
+            temp_y_hat
+            for col in data_cols:
+                for j, element in enumerate(X[col]):
+                    below = X[X[col] <= element]
+                    above = X[X[col] > element]
+                    if len(above) == 0:
+                        continue
+
+
+    def predict(self, X):
+        X = self._check_x_data_type(X)
+
+    def score(self, y, y_hat):
+        return R2(y, y_hat)
+
+    def _check_x_data_type(self, X):
+        if not isinstance(X, pd.core.frame.DataFrame):
+            return pd.DataFrame(X)
+        else:
+            return X
+
+    def _generate_y_hat(self):
