5229e60b644a58593713a85b5e6eef340d3953ce
nickbuker <nickbuker@gmail.com>
2017-10-24 17:40:04 -0700

    add updates to fit method

--- src/DecisionTreeRegressor.py
+++ src/DecisionTreeRegressor.py
@@ -1,3 +1,4 @@
+# TODO min leaf size? Fix Index error if too deep.
 import numpy as np
 import pandas as pd
 from scoring import R2
@@ -11,23 +12,13 @@ class DecisionTreeRegressor:
 
     def fit(self, X, y, max_depth):
         self.tree = Tree()
-        self.rules = []
-        X = self._check_x_data_type(X)
-        X['y'] = y
-        X['y_hat'] = np.mean(y)
-        X = X.sort_values(by='y')
-        X = X.reset_index(drop=True)
-        data_cols = [col for col in X.columns if col not in ['y', 'y_hat']]
-        for _ in range(max_depth):
-            score = [0, data_cols[0], -1]
-            temp_y_hat
-            for col in data_cols:
-                for j, element in enumerate(X[col]):
-                    below = X[X[col] <= element]
-                    above = X[X[col] > element]
-                    if len(above) == 0:
-                        continue
-
+        self.max_depth = max_depth
+        data = self._check_x_data_type(X)
+        data.loc[:, 'y'] = y
+        data_cols = [col for col in data.columns if col != 'y']
+        self._find_best_feature(data=data,
+                                data_cols=data_cols,
+                                tree=self.tree.tree)
 
     def predict(self, X):
         X = self._check_x_data_type(X)
@@ -41,11 +32,44 @@ class DecisionTreeRegressor:
         else:
             return X
 
-    def _find_best_feature(self):
-        pass
+    def _find_best_feature(self, data, data_cols, tree, k='root', i=1):
+        results = [-np.inf]
+        for col in data_cols:
+            temp_results = self._find_best_split(col=data[col].values,
+                                                 y=data['y'].values)
+            if temp_results[-1] > results[-1]:
+                results = [col] + temp_results  # concat the lists together
+        if i == self.max_depth:
+            tree[k] = [results[0], results[3], {'b': results[1], 'a': results[2]}]
+        else:
+            tree[k] = [results[0], results[3], {}]
+            self._find_best_feature(data=data[data[results[0]] <= results[3]],
+                                    data_cols=data_cols,
+                                    tree=tree[k][-1],
+                                    k='b',
+                                    i=i + 1)
+            self._find_best_feature(data=data[data[results[0]] > results[3]],
+                                    data_cols=data_cols,
+                                    tree=tree[k][-1],
+                                    k='a',
+                                    i=i + 1)
 
-    def _find_best_split(self):
-        pass
+    def _find_best_split(self, col, y):
+        results = [-np.inf]
+        vals = set(col)
+        for val in vals:
+            mask_b = col <= val
+            mean_b = np.mean(y[mask_b])
+            mean_a = np.mean(y[col > val])
+            if np.isnan(mean_b) or np.isnan(mean_a):
+                continue
+            else:
+                y_hat = np.repeat(mean_a, len(y))
+                y_hat[mask_b] = mean_b
+                temp_score = self.score(y, y_hat)
+                if temp_score > results[-1]:
+                    results = [mean_b, mean_a, val, temp_score]
+        return results
 
     def _generate_y_hat(self):
         pass
