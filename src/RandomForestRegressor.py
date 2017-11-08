5e0f70b787dc83a12b6d17bbc6762dc508cac98b
Buker, Nick <Nick.Buker@nordstrom.com>
2017-11-08 10:28:49 -0800

    add type checks to Random Forest

--- src/RandomForestRegressor.py
+++ src/RandomForestRegressor.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 from scoring import R2, RSS
 from DecisionTreeRegressor import DecisionTreeRegressor
 
@@ -28,11 +29,13 @@ class RandomForestRegressor:
         # instantiate forest of decision trees
         self.trees = [DecisionTreeRegressor() for _ in range(n_estimators)]
         # create masks for rows and columns of data for each tree
+        X = self._check_data_type_X(X)
+        y = self._check_data_type_y(y)
         rows = [self._sample_rows(X.index) for _ in range(n_estimators)]
         n_cols = int(len(X.columns) ** 0.5)
         cols = [self._sample_cols(X.columns, n_cols) for _ in range(n_estimators)]
         for i, tree in enumerate(self.trees):
-            tree.fit(X[cols[i]].iloc[rows[i]], y[rows[i]])
+            tree.fit(X[cols[i]].iloc[rows[i]], y[rows[i]], max_depth)
 
     def predict(self, X):
         """
@@ -45,6 +48,7 @@ class RandomForestRegressor:
         -------
 
         """
+        X = self._check_data_type_X(X)
         preds = []
         for tree in self.trees:
             preds.append(tree.predict(X))
@@ -62,8 +66,40 @@ class RandomForestRegressor:
         -------
 
         """
+        X = self._check_data_type_X(X)
+        y = self._check_data_type_y(y)
         y_hat = self.predict(X)
         return R2(y, y_hat)
+
+    def _check_data_type_X(self, X):
+        """ Checks if X is a pandas DataFrame and if not, converts it to one
+        Parameters
+        ----------
+        X : pandas DataFrame or numpy array
+            data
+        Returns
+        -------
+        pandas DataFrame
+            data of appropriate type
+        """
+        if not isinstance(X, pd.core.frame.DataFrame):
+            X = pd.DataFrame(X)
+        return X
+
+    def _check_data_type_y(self, y):
+        """ Checks if y is a pandas Series and if not, converts it to one
+        Parameters
+        ----------
+        y : pandas Series or numpy array
+            data
+        Returns
+        -------
+        pandas Series
+            data of appropriate type
+        """
+        if not isinstance(y, pd.core.series.Series):
+            y = pd.Series(y)
+        return y
 
     def _sample_rows(self, index):
         """
@@ -91,4 +127,3 @@ class RandomForestRegressor:
 
         """
         return np.random.choice(cols, n_cols)
-
