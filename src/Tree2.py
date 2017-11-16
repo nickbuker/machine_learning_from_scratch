aed0d772ed04fe63258d463f1f6ce7885dc83008
Buker, Nick <Nick.Buker@nordstrom.com>
2017-11-16 09:51:28 -0800

    update node class in Tree2

--- src/Tree2.py
+++ src/Tree2.py
@@ -1,7 +1,30 @@
+class Node:
+
+    def __init__(self, data=None, depth=0, is_leaf=False):
+        """ A general node class for generating tree models
+
+        Parameters
+        ----------
+        data : Any type
+            data saved in node typically:
+                - tuple (split col, split value)
+                - float with y_hat if leaf
+        depth : int
+            depth of node (used for tree termination)
+        is_leaf : bool
+            flags whether or not node is a terminal leaf of the tree
+        """
+        self.a = None
+        self.b = None
+        self.data = data
+        self.depth = depth
+        self.is_leaf = is_leaf
+
+
 class Tree:
 
     def __init__(self):
-        self.tree = {'root': {}}
+        self.tree = Node()
 
     def add_node(self):
         pass
